49b8af5340213ed123c1b7e33bf84218f5318c2d
Radek <radoslaw.babinski1@microsoft.wsei.edu.pl>
2023-11-06 20:37:26 +0100

    Usuniecie wadliwych plików

--- Poject_Try_two/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Poject_Try_two.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Laboratorium3zadanie
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
